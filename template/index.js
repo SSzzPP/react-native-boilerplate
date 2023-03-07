97e3df0f4541ce35ba4c0dd32f5e047d5fbea76e
Darkce <darkce97@gmail.com>
2023-03-07 08:47:10 +0800

    fix: Unable to resolve module ../app.json

--- template/index.js
+++ template/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 
 import App from './src/App';
-import { name } from '../app.json';
+import { name } from './app.json';
 
 AppRegistry.registerComponent(name, () => App);
